20107a13cab1e0133126a464ae0fd96bbfaa238f
vrde <alberto@granzotto.net>
2024-04-02 18:23:50 +0200

    Add `to` field to redeem function (#7)
    Close #6

--- contracts/InternalMarket/InternalMarket.sol
+++ contracts/InternalMarket/InternalMarket.sol
@@ -97,8 +97,8 @@ contract InternalMarket is Initializable, HasRole, InternalMarketBase {
      * @dev Redeem the locked tokens.
      * @param amount The amount of tokens to redeem.
      */
-    function redeem(uint amount) public {
-        _redeem(_msgSender(), amount);
+    function redeem(address to, uint amount) public {
+        _redeem(to, amount);
     }
 
     /**

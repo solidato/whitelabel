ca6783e306bd7f5c3b08cb413dcd604dcccfa458
Nicola Miotto <miotto@posteo.de>
2024-05-02 16:59:19 +0200

    deploy to optimism

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,28 +11,13 @@ dotEnvConfig();
 
 import("./tasks").catch((e) => console.log("Cannot load tasks", e.toString()));
 
-const BLAST_API_KEY = process.env.BLAST_API_KEY || "";
-const MUMBAI_PRIVATE_KEY =
-  process.env.MUMBAI_PRIVATE_KEY! ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
-const POLYGON_PRIVATE_KEY =
-  process.env.POLYGON_PRIVATE_KEY! ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
-const TEVMOS_PRIVATE_KEY =
-  process.env.TEVMOS_PRIVATE_KEY! ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
-const EVMOS_PRIVATE_KEY =
-  process.env.EVMOS_PRIVATE_KEY! ||
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const CRONOSCAN_API_KEY = process.env.CRONOSCAN_API_KEY;
 const COINMARKETCAP_KEY = process.env.COINMARKETCAP_KEY || "";
-const TOKEN = process.env.TOKEN || "CRO";
-const GASPRICE_API = {
-  MATIC: "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice",
-  ETH: "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
-  CRO: `https://api.cronoscan.com/api?module=proxy&action=eth_gasPrice&apiKey=${CRONOSCAN_API_KEY}`,
-}[TOKEN];
+const GASPRICE_API = `https://api-optimistic.etherscan.io/api?module=proxy&action=eth_gasPrice&apiKey=${ETHERSCAN_API_KEY}`;
 const GASREPORT_FILE = process.env.GASREPORT_FILE || "";
 const NO_COLORS = process.env.NO_COLORS == "false" || GASREPORT_FILE != "";
 const GAS_PRICE = process.env.GAS_PRICE
@@ -64,51 +49,20 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
-    mumbai: {
-      //url: `https://polygon-testnet.blastapi.io/${BLAST_API_KEY}`,
-      url: "https://polygon-mumbai-bor.publicnode.com",
-      accounts: [MUMBAI_PRIVATE_KEY],
-    },
-    polygon: {
-      url: "https://polygon-rpc.com/",
-      accounts: [POLYGON_PRIVATE_KEY],
-    },
-    tevmos: {
-      url: "https://evmos-testnet.lava.build",
-      accounts: [TEVMOS_PRIVATE_KEY],
-    },
-    evmos: {
-      url: "https://eth.bd.evmos.org:8545",
-      accounts: [EVMOS_PRIVATE_KEY],
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
     },
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
-    customChains: [
-      {
-        network: "evmos",
-        chainId: 9001,
-        urls: {
-          apiURL: "https://escan.live/api",
-          browserURL: "https://escan.live",
-        },
-      },
-      {
-        network: "tevmos",
-        chainId: 9000,
-        urls: {
-          apiURL: "https://testnet.escan.live/api",
-          browserURL: "https://testnet.escan.live",
-        },
-      },
-    ],
   },
   gasReporter: {
     currency: "EUR",
     coinmarketcap: COINMARKETCAP_KEY,
     enabled: process.env.REPORT_GAS ? true : false,
     gasPriceApi: GASPRICE_API,
-    token: TOKEN,
+    token: "OP",
     gasPrice: GAS_PRICE,
     outputFile: GASREPORT_FILE,
     noColors: NO_COLORS,
